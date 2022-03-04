110542a9ae037e80777806fa80a9a9e5c2666144
Samuel <samuel.cflau@gmail.com>
2022-03-04 16:45:32 +0800

    121. Add Routes & Controller 6 分鐘 (’routes’, userRoutes.js; ‘controllers’, userController.js)

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
   res.status(200).json('hello world!')
 })
 
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })

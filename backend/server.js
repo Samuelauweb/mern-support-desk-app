5f40908a30dc06fd227d52cbfc089be81b740a39
Samuel <samuel.cflau@gmail.com>
2022-03-04 15:52:47 +0800

    120. Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).json('hello world!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
